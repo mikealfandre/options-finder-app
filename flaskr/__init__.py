7b34c62c8d24545db34b9fa3c843dbf335e3973e
Mike Alfandre <mikealfandre@gmail.com>
2021-04-12 19:39:09 -0600

    Finish tutorial. Get stock data

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,8 @@
 import os
-
 from flask import Flask
-
 from . import db
+from . import auth
+from . import blog
 
 
 def create_app(test_config=None):
@@ -32,5 +32,8 @@ def create_app(test_config=None):
         return 'Hello, World!'
 
     db.init_app(app)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
